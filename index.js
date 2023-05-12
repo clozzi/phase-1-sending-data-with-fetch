43a8f4dc20fe36dce00b1a6b6f3e91cf5ba06fef
Christopher Lozzi <clozzi12@gmail.com>
2023-05-12 12:42:15 -0400

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(response => response.json())
+        .then((object) => {document.body.innerHTML = object.id})
+        .catch(function (error) {
+            console.log(error.message);
+            document.querySelector("body").append(error.message);
+        });
+}
+
+
+
+
+
